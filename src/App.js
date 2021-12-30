1d9ebe538616c42b9d9b24d18d75db2ba72e9217
Peter Thao <peterthao@Peters-iMac.local>
2021-12-30 15:18:33 -0500

    Modal complete and add gh pages

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
-
   const [categories] = useState([
-    { name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects'
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', 
-      description: 'Portraits of people in my life' 
-    },
-    { name: 'food', 
-    description: 'Delicious delicacies' 
-    },
-    { name: 'landscape', 
-    description: 'Fields, farmhouses, waterfalls, and the beauty of nature' 
-    }
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -36,13 +31,12 @@ function App() {
       <main>
         {!contactSelected ? (
           <>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
           </>
-        ):(
+        ) : (
           <ContactForm></ContactForm>
         )}
-      
       </main>
     </div>
   );
